fcac53b56f4f3765681c63467c81fcbaede80db8
Lewis Hotchkiss <125033935+LewisHotchkissDPUK@users.noreply.github.com>
2024-02-04 18:32:58 +0000

    u

--- Home.py
+++ Home.py
@@ -16,3 +16,14 @@ with col2:
     st.write('''This is an AI risk learning platform to help you understand vulnerabilities/concerns in AI models and what techniques 
              you can implement in your code to mitigate them. Through this course, wou will learn how to implement privacy-preserving techniques
              including: differential privacy, synthetic data, and homomorphic encryption.''')
+    
+course1, course2, course3, course4 = st.columns(4)
+
+with course1:
+    st.image('Picture1.png')
+with course2:
+    st.image('Picture2.png')
+with course3:
+    st.image('Picture3.png')
+with course4:
+    st.image('Picture4.png')
