39b3b0a06efb148e07713b692bc11c15848ce2db
Lewis Hotchkiss <125033935+LewisHotchkissDPUK@users.noreply.github.com>
2024-02-04 18:04:59 +0000

    updated

--- Home.py
+++ Home.py
@@ -6,14 +6,13 @@ st.set_page_config(
     layout="wide",
 )
 
-st.title("AI Risk Learning Platform")
-
 col1, col2 = st.columns(2)
 
 with col1:
     st.image('Safe-cuate.png')
 
 with col2:
+    st.title("AI Risk Learning Platform")
     st.write('''This is an AI risk learning platform to help you understand vulnerabilities/concerns in AI models and what techniques 
              you can implement in your code to mitigate them. Through this course, wou will learn how to implement privacy-preserving techniques
              including: differential privacy, synthetic data, and homomorphic encryption.''')
