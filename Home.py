86f21f72a2402b6ed9ca8a29b165358c5e77f8a2
Lewis Hotchkiss <125033935+LewisHotchkissDPUK@users.noreply.github.com>
2024-02-04 18:49:34 +0000

    u

--- Home.py
+++ Home.py
@@ -20,9 +20,7 @@ with col2:
     st.title("AI Risk Learning Platform")
     st.write('''This is an AI risk learning platform to help you understand vulnerabilities/concerns in AI models and what techniques 
              you can implement in your code to mitigate them. Through this course, wou will learn how to implement privacy-preserving techniques
-             including: differential privacy, synthetic data, and homomorphic encryption.
-             
-             Throughout the course, you will be able to try practical activities to implement some of these techniques yourself
+             including: differential privacy, synthetic data, and homomorphic encryption. Throughout the course, you will be able to try practical activities to implement some of these techniques yourself
              and run built-in attacks to evaluate their effectiveness.''')
     
 st.write("")
