4de27d5b4540fe61d19b60233c18aab088bc39c3
Lewis Hotchkiss <125033935+LewisHotchkissDPUK@users.noreply.github.com>
2024-02-04 15:44:59 +0000

    updated

--- pages/0_Animation_Demo.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.set_page_config(
-    page_title="Home",
-    page_icon="👋",
-)
-
-st.title("Synthetic Data Evaluation")
---
+++ pages/0_Practice_Envrionent.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Practice Environment",
+    page_icon="👋",
+)
+
+st.title("Practice Environment")
+
+jupyterlite(1500, 1600)
