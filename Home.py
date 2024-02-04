224ad2c1c75f643e830730dc3d269cebea53ce8f
Lewis Hotchkiss <125033935+LewisHotchkissDPUK@users.noreply.github.com>
2024-02-04 18:02:25 +0000

    updated

--- Home.py
+++ Home.py
@@ -12,3 +12,6 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.image('Safe-cuate.png')
+
+with col2:
+    st.write("This is an AI risk learning platform to help you understand vulnerabilities in AI models and what techniques you can implement in your code to mitigate them")
